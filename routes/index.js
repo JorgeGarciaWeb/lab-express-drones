a787a1f1cfb20e2fb54fe6f5e4f0b9d2a15186e7
Jorge Garcia <jorge.garcia.camp@outlook,com>
2022-06-25 07:34:57 +0200

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const Drone = require('./../models/Drone.model.js');
+
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
